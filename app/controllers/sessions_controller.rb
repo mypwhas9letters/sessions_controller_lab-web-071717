38ac69fbe5622a7c798f67f1ce207201cf04bfed
Hui Wang <huiw3939@gmail.com>
2017-08-18 14:23:57 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+def new
+end
+
+def create
+  if params[:name] && !params[:name].empty?
+		session[:name] = params[:name]
+  		redirect_to '/'
+  	else
+  		redirect_to '/login'
+  	end
+end
+
+def destroy
+  session[:name] = nil
+  redirect_to '/'
+
+end
+
+end
